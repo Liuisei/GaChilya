c86d8510f1503278d6c916093f97721c56cf5a05
a0966891512@gmail.com <a0966891512@gmail.com>
2024-04-09 03:01:30 +0900

    [add] Manager

---
+++ Assets/Manager/DataManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : Singleton<DataManager>
+{
+    private int _money;
+    
+    public override void AwakeFunction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
